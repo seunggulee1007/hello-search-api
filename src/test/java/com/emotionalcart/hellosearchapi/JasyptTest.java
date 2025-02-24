e3157d5f76ba626a98076e72db656a0d0f264e39
이승구 <leesg107@naver.com>
2025-02-24 22:07:33 +0900

    feat: jasypt 추가

---
+++ src/test/java/com/emotionalcart/hellosearchapi/JasyptTest.java
@@ -0,0 +1,29 @@
+package com.emotionalcart.hellosearchapi;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class JasyptTest {
+
+    @Autowired
+    private StringEncryptor jasyptStringEncryptor;
+
+    @Test
+    @DisplayName("암호화 테스트")
+    void encrypt () {
+        // given
+        String text = "이너서클 이커머스";
+        // when
+        String encryptedText = jasyptStringEncryptor.encrypt(text);
+        String decryptedText = jasyptStringEncryptor.decrypt(encryptedText);
+        // then
+        assertThat(text).isEqualTo(decryptedText);
+
+    }
+}
