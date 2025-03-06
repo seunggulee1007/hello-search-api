114631c984a86a033110965e6017712d3c88691c
이승구 <leesg107@naver.com>
2025-03-06 15:55:59 +0900

    feat: 배너 단건 저장 로직 추가

--- src/main/java/com/emotionalcart/hellosearchapi/presentation/banner/BannerSaveController.java
+++ src/main/java/com/emotionalcart/hellosearchapi/presentation/banner/BannerSaveController.java
@@ -1,7 +1,11 @@
 package com.emotionalcart.hellosearchapi.presentation.banner;
 
+import com.emotionalcart.hellosearchapi.application.banner.BannerSaveRequest;
 import com.emotionalcart.hellosearchapi.application.banner.BannerSaveService;
+import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +16,16 @@ public class BannerSaveController {
 
     private final BannerSaveService bannerSaveService;
 
+    @Hidden
+    @PostMapping("/all")
     public void saveBannersAll() {
         bannerSaveService.saveAll();
+    }
+
+    @PostMapping
+    @Operation(summary = "배너 저장", description = "배너를 저장합니다.")
+    public void saveBanner(BannerSaveRequest request) {
+        bannerSaveService.save(request);
     }
 
 }
